f5fe071f5b030f87f1ebd23eb3541b3b1927ac2b
Kipp Ashford <kipp@seven2.com>
2019-07-12 12:07:19 -0700

    Add proper type signatures

--- src/handlers/App.ts
+++ src/handlers/App.ts
@@ -1,11 +1,17 @@
-import { APIGateway } from "aws-sdk";
+import { APIGatewayEvent, Context, ProxyResult } from "aws-lambda";
 
-export const handler = async (event :any, context :any) :Promise<APIGateway.MethodResponse> => {
+/**
+ * 
+ * @param event The API gateway event
+ * @param context 
+ */
+export const handler = async (event :APIGatewayEvent, context :Context) :Promise<ProxyResult> => {
 
     console.log(`Got an environment variable: ${process.env.EXAMPLE_VAR}`);
 
-    const res :APIGateway.MethodResponse = {
-        statusCode: "200"
+    const res :ProxyResult = {
+        statusCode: 200,
+        body: 'success!'
     }
     return res;
 }
